b7a8d21e529796cfea3a7cca2b522495ca7333e6
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2022-01-13 17:52:27 +1300

    rename vars to fit convention

--- getpaid.js
+++ getpaid.js
@@ -1,9 +1,14 @@
 /** @param {import(".").NS} ns */
-export async function get_paid(ns, target, moneyThreshold, securityThreshold) {
+export async function get_paid(
+  ns,
+  target,
+  money_threshold,
+  security_threshold
+) {
   while (true) {
-    if (ns.getServerSecurityLevel(target) > securityThreshold) {
+    if (ns.getServerSecurityLevel(target) > security_threshold) {
       await ns.weaken(target);
-    } else if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
+    } else if (ns.getServerMoneyAvailable(target) < money_threshold) {
       await ns.grow(target);
     } else {
       await ns.hack(target);
